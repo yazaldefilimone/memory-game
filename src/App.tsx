b75f6d1ac724941e5a4aaddea53c1f31dc3f4885
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-23 11:11:50 +0200

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Fragment, FunctionComponent } from "react";
+
+import GlobalStyles from "./styles/globalStyle";
+
+export const App: FunctionComponent = () => {
+  return (
+    <Fragment>
+      <GlobalStyles />
+    </Fragment>
+  );
+};

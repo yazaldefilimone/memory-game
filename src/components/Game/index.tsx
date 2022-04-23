c71f7e156e24649f0b5a560b7cd9a15652256561
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-23 11:23:48 +0200

    feat: create frist templete components

---
+++ src/components/Game/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+import { Container } from "./styles";
+
+export const Game: FunctionComponent = () => {
+  return (
+    <div>
+      <h2>Game</h2>
+    </div>
+  );
+};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+import { Container } from "./styles";
+
+export const Header: FunctionComponent = () => {
+  return (
+    <div>
+      <h2>Header</h2>
+    </div>
+  );
+};
